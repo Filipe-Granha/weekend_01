c59f4e0077b6674f7d89cce24f664ff629f86101
Filipe <granha.carlos@gmail.com>
2017-05-28 14:28:13 +0100

    test_5

--- pet_shop.rb
+++ pet_shop.rb
@@ -4,12 +4,9 @@ end
 
 
 
-
-
 def total_cash(cash)
   cash[:admin][:total_cash]
 end
-
 
 
 
@@ -18,9 +15,13 @@ def total_cash(add_cash, x)
 end
 
 
+
 def total_cash(add_cash, x)
   add_cash[:admin][:total_cash] -= x
 end
 
 
 
+def pets_sold (sold_pets)
+  sold_pets[:admin][:pets_sold]
+end
